5c4f381c9c2ba75e097fc38005f11619fe87f482
tidaraty <frankpook1978@gmail.com>
2023-04-02 19:30:33 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution in this file!
+const employee ={
+    name:"Jennifer",
+    StressAddress:"234 main st"
+};
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {
+        ...employee,
+        [key]:value
+    };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key]=value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {employee};
+    employee[key] ="Sam";
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+     delete employee[key];
+     return employee;
+}
+
+    
+
+
+
+
+
+
+    
+
+
+
+
+    
+
+
+
+
+
+
+
